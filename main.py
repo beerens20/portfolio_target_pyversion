82f6c79b78054f714c4b3de8a66335a2ab027449
Mike Beerens <beerens20@gmail.com>
2025-12-26 11:51:14 -0500

    Added placeholder functions and laid out inputs/outputs in columns

--- main.py
+++ main.py
@@ -2,8 +2,35 @@ import streamlit as st
 
 def main():
     st.title("Rebalance Your Portfolio")
-    x = st.slider("Select a value")
+    col1, col2 = st.columns(2, gap="large", border=True)
+   
+
+    with col1:
+        age = st.slider("How old are you?", 18,100)
+        domestic = st.text_input("Domestic mutual funds ($):", value="0")
+        international = st.text_input("International mutual funds ($)", value="0")
+        bonds = st.text_input("Bonds ($)", value="0")
+
+    with col2:
+        st.markdown("**Recommended Target Allocation**", width="content")
+        st.divider()
+        domestic_output = calculate_domestic(age, domestic)
+        international_output = calculate_international(age, international)
+        bonds_output = calculate_bonds(age, bonds)
+        st.markdown(domestic_output)
+        st.markdown(international_output)
+        st.markdown(bonds_output)
+        
     
+
+def calculate_domestic(domestic, age):
+    return "Domestic Target: $75,000 (70%)"
+
+def calculate_international(international, age):
+    return "International Target: $75,000 (70%)"
+
+def calculate_bonds(bonds, age):
+    return "Bonds Target: $75,000 (70%)"
 
 if __name__ == "__main__":
     main()
